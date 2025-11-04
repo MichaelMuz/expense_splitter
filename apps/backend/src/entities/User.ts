7286f387c8fadb1836c9026bb11cf3c116d6f9d4
Michael Muz <20muzm01@gmail.com>
2025-11-04 08:36:27 -0500

    prog

--- apps/backend/src/entities/User.ts
+++ apps/backend/src/entities/User.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, Generated, CreateDateColumn, PrimaryColumn, ManyToOne, JoinColumn } from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +10,43 @@ export class User {
 
     @Column('varchar', { unique: true })
     email!: string;
+}
+
+@Entity()
+export class Group {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column('varchar')
+    name!: string;
+
+    @Column('varchar', { unique: true })
+    @Generated('uuid')
+    inviteCode!: string;
+
+    @CreateDateColumn({ type: 'timestamptz' })
+    createdAt!: Date
+}
+
+@Entity()
+export class GroupMembership {
+    @PrimaryColumn()
+    UserId!: number;
+
+    @PrimaryColumn()
+    GroupId!: number;
+
+    @Column('varchar')
+    role!: string;
+
+    @CreateDateColumn({ type: 'timestamptz' })
+    joinedAt!: Date
+
+    @ManyToOne(() => User, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'user_id' })
+    user!: User;
+
+    @ManyToOne(() => Group, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'group_id' })
+    group!: Group;
 }
