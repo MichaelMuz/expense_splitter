d49d1c49a71ee62857f8ed9fcbfc9a68e573d451
Michael Muz <20muzm01@gmail.com>
2025-11-03 22:33:29 -0500

    scripts and env

--- apps/backend/src/data-source.ts
+++ apps/backend/src/data-source.ts
@@ -2,13 +2,19 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { User } from './entities/User.js';
 
+const { POSTGRES_PASSWORD, POSTGRES_DB } = process.env;
+
+if (!POSTGRES_PASSWORD || !POSTGRES_DB) {
+    throw new Error('Missing required environment variables');
+}
+
 export const AppDataSource = new DataSource({
     type: 'postgres',
     host: 'localhost',
     port: 5432,
     username: 'postgres',
-    password: 'postgres',
-    database: 'expense_splitter',
+    password: POSTGRES_PASSWORD,
+    database: POSTGRES_DB,
     synchronize: true,
     logging: true,
     entities: [User],
