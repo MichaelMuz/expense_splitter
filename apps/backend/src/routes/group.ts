b67cc018bfe81bc166221d83d16b41279a748909
Michael Muz <20muzm01@gmail.com>
2025-11-04 22:28:36 -0500

    prog

--- apps/backend/src/routes/group.ts
+++ apps/backend/src/routes/group.ts
@@ -2,15 +2,16 @@ import Router from '@koa/router';
 import { AppDataSource } from '../data-source.js';
 import { Group, GroupMembership } from '../entities/User.js';
 
-const router = new Router({ prefix: '/api/users' });
+const router = new Router({ prefix: '/api/groups' });
 const groupMembershipRepository = AppDataSource.getRepository(GroupMembership);
 const groupRepository = AppDataSource.getRepository(Group);
 
-router.get('/:id/groups', async (ctx) => {
-    const userId = Number(ctx.params['id'])
+router.get('/', async (ctx) => {
+    const userId = Number(ctx.state['user']['id'])
     if (isNaN(userId)) {
-        ctx.status = 400
-        ctx.body = { error: 'Invalid Id' }
+        ctx.status = 500
+        // Impossible state
+        ctx.body = { error: 'Internal Error' }
         return
     }
     const memberships = await groupMembershipRepository.find({
@@ -22,10 +23,33 @@ router.get('/:id/groups', async (ctx) => {
     ctx.body = groups
 })
 
-router.post('/groups', async (ctx) => {
-    const groupName = ctx.request.body as {name: string}
-    const group = groupRepository.create(groupName)
+router.post('/', async (ctx) => {
+    const userId = Number(ctx.state['user']['id'])
+    if (isNaN(userId)) {
+        ctx.status = 500
+        // Impossible state
+        ctx.body = { error: 'Internal Error' }
+        return
+    }
+    const { name: groupName } = ctx.request.body as { name: string }
+    if (!groupName || typeof groupName !== 'string' || groupName.trim().length === 0) {
+        ctx.status = 400;
+        ctx.body = { error: 'Group name is required.' };
+        return;
+    }
+
+    const group = groupRepository.create({name:groupName})
     await groupRepository.save(group)
+
+    const groupMembership = groupMembershipRepository.create({ userId, groupId: group.id, role: 'admin' })
+    await groupMembershipRepository.save(groupMembership)
+
+    ctx.status = 201
+    ctx.body = {
+        message: 'Group created successfully',
+        group: group,
+        membership: groupMembership
+    };
 })
 
 export default router;
