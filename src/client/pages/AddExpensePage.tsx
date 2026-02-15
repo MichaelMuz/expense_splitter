ed3be89e661ce8e38c0c99632cb7afcff1f9acd6
Michael Muz <20muzm01@gmail.com>
2026-02-15 18:21:03 -0500

    better expense entering user experience

--- src/client/pages/AddExpensePage.tsx
+++ src/client/pages/AddExpensePage.tsx
@@ -4,21 +4,26 @@ import { Layout } from '../components/layout/Layout';
 import { Loading } from '../components/layout/Loading';
 import ExpenseForm from '../components/expenses/ExpenseForm';
 import { useCreateExpense } from '../hooks/useExpenses';
+import type { CreateExpenseInput } from '@/shared/schemas/expense';
 
 function AddExpenseCore({ groupId }: { groupId: string }) {
   const navigate = useNavigate();
   const { data: group, isLoading } = useGroup(groupId);
-  const mutation = useCreateExpense(groupId)
+  const createExpense = useCreateExpense(groupId)
+
+  const onSubmit = (expense: CreateExpenseInput) => {
+    createExpense.mutate(expense, { onSuccess: () => navigate(`/groups/${groupId}`) })
+  }
 
   if (isLoading) return <Loading name="group" />;
   else if (!group) return <Navigate to="/groups" replace />;
 
   return (
     <Layout>
-      {mutation.isError && <p>{mutation.error.message}</p>}
+      {createExpense.isError && <p>{createExpense.error.message}</p>}
       <button onClick={() => navigate(`/groups/${groupId}`)}>Back</button>
       <h1>Add Expense to {group.name}</h1>
-      <ExpenseForm members={group.members} isPending={mutation.isPending} onSubmit={(expense => mutation.mutate(expense))} />
+      <ExpenseForm members={group.members} isPending={createExpense.isPending} onSubmit={onSubmit} />
     </Layout>
   );
 
