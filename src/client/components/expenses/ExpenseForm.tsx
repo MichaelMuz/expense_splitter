e9ab6397c73d180cc5c701b0cfcc822c7c246b41
Michael Muz <20muzm01@gmail.com>
2026-02-15 19:21:37 -0500

    include tax and tip into expense forms

--- src/client/components/expenses/ExpenseForm.tsx
+++ src/client/components/expenses/ExpenseForm.tsx
@@ -1,3 +1,4 @@
+import { $Enums } from '@prisma/client';
 import type { CreateExpenseInput, Expense } from "@/shared/schemas/expense";
 import { useState } from "react";
 import type { Group } from "@/shared/schemas/group";
@@ -7,15 +8,28 @@ export default function ExpenseForm({ initialData, members, isPending, onSubmit 
     const [name, setName] = useState(initialData?.name || "");
     const [description, setDescription] = useState(initialData?.description || "");
     const [baseAmount, setBaseAmount] = useState(initialData?.baseAmount ? toDollars(initialData?.baseAmount).toString() : "");
+    const [taxAmount, setTaxAmount] = useState(initialData?.taxAmount ? toDollars(initialData?.taxAmount).toString() : "");
+    const [taxType, setTaxType] = useState(initialData?.taxType || null);
+    const [tipAmount, setTipAmount] = useState(initialData?.tipAmount ? toDollars(initialData?.tipAmount).toString() : "");
+    const [tipType, setTipType] = useState(initialData?.tipType || null);
     const [payerIds, setPayerIds] = useState(initialData?.payers.map(p => p.groupMemberId) || []);
     const [owerIds, setOwerIds] = useState(initialData?.owers.map(p => p.groupMemberId) || []);
 
     const handleSubmit = (e: React.FormEvent) => {
+        const taxTypeAmount = taxType ? {
+            taxType, taxAmount: toCents(parseFloat(taxAmount))
+        } : {};
+        const tipTypeAmount = tipType ? {
+            tipType, tipAmount: toCents(parseFloat(tipAmount))
+        } : {};
+
         e.preventDefault();
         onSubmit({
             name,
             description,
             baseAmount: toCents(parseFloat(baseAmount)),
+            ...taxTypeAmount,
+            ...tipTypeAmount,
             payers: payerIds.map(id => ({
                 groupMemberId: id,
                 splitMethod: 'EVEN',
@@ -24,7 +38,6 @@ export default function ExpenseForm({ initialData, members, isPending, onSubmit 
                 groupMemberId: id,
                 splitMethod: 'EVEN',
             })),
-
         })
     }
 
@@ -65,7 +78,7 @@ export default function ExpenseForm({ initialData, members, isPending, onSubmit 
                     <input
                         type="checkbox"
                         checked={payerIds.includes(m.id)}
-                        onChange={(e) => setPayerIds(e.target.checked ? [...payerIds, m.id] : payerIds.filter(id => id !== m.id))}
+                        onChange={e => setPayerIds(e.target.checked ? [...payerIds, m.id] : payerIds.filter(id => id !== m.id))}
                     />
                     {m.name}
                 </label>
@@ -77,11 +90,51 @@ export default function ExpenseForm({ initialData, members, isPending, onSubmit 
                     <input
                         type="checkbox"
                         checked={owerIds.includes(m.id)}
-                        onChange={(e) => setOwerIds(e.target.checked ? [...owerIds, m.id] : owerIds.filter(id => id !== m.id))}
+                        onChange={e => setOwerIds(e.target.checked ? [...owerIds, m.id] : owerIds.filter(id => id !== m.id))}
                     />
                     {m.name}
                 </label>
             )}
+
+            <p>Tax</p>
+            {[...Object.values($Enums.TaxTipType), null].map(t =>
+                <label key={t}>
+                    <input
+                        type="radio"
+                        checked={taxType == t}
+                        onChange={() => setTaxType(t)}
+                    />
+                    {t || "None"}
+                </label>)}
+            {taxType &&
+                <label >Tax Amount
+                    <input
+                        type="text"
+                        value={taxAmount}
+                        onChange={(e) => setTaxAmount(e.target.value)}
+                        required
+                    />
+                </label>}
+
+            <p>Tip</p>
+            {[...Object.values($Enums.TaxTipType), null].map(t =>
+                <label key={t}>
+                    <input
+                        type="radio"
+                        checked={tipType == t}
+                        onChange={() => setTipType(t)}
+                    />
+                    {t || "None"}
+                </label>)}
+            {tipType &&
+                <label >Tip Amount
+                    <input
+                        type="text"
+                        value={tipAmount}
+                        onChange={(e) => setTipAmount(e.target.value)}
+                        required
+                    />
+                </label>}
 
             <button type='submit' disabled={isPending}>Submit</button>
         </form>
