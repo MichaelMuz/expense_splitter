106e0ab146067acfc91f1ade6d0d233b413ce044
Michael Muz <20muzm01@gmail.com>
2026-02-16 22:39:49 -0500

    sign up

--- src/client/pages/SignupPage.tsx
+++ src/client/pages/SignupPage.tsx
@@ -1,40 +1,51 @@
-import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import { Layout } from '../components/layout/Layout';
 
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { signupSchema } from '@/shared/schemas/auth';
+import { z } from 'zod';
+
+const signupFormSchema = signupSchema.extend({
+  confirmPassword: z.string(),
+}).refine(data => data.password === data.confirmPassword, { message: "Passwords must match", path: ["confirmPassword"] });
+type SignupForm = z.infer<typeof signupFormSchema>;
+
 export default function SignupPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
   const { signupMutation } = useAuth();
+
+  const { register, handleSubmit, formState: { errors } } = useForm<SignupForm>({
+    resolver: zodResolver(signupFormSchema)
+  })
   const navigate = useNavigate();
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    signupMutation.mutate({ email, password }, {
-      onSuccess: () => navigate('/groups'),
-    });
+  const onSubmit = (data: SignupForm) => {
+    const { confirmPassword, ...signupData } = data;
+    signupMutation.mutate(signupData, { onSuccess: () => navigate('/groups') });
   };
 
   return (
     <Layout>
       <h1>Sign Up</h1>
       {signupMutation.isError && <p>{signupMutation.error.message}</p>}
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label>Email
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <input type="email" {...register("email")} />
+            {errors.email && <p>{errors.email.message}</p>}
           </label>
         </div>
         <div>
           <label>Password
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <input type="password" {...register("password")} />
+            {errors.password && <p>{errors.password.message}</p>}
           </label>
         </div>
         <div>
           <label>Confirm Password
-            <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
+            <input type="password" {...register("confirmPassword")} />
+            {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
           </label>
         </div>
         <button type="submit" disabled={signupMutation.isPending}>
